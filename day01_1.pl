50df0c543b5ee867555f2ca965d2017d70be4017
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2017-12-03 14:28:07 +0100

    Day 01 - first half of this day puzzle is complete
    Signed-off-by: Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>

---
+++ day01_1.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+# Álvaro Castellano Vela - 03/12/2017
+
+use strict;
+use warnings;
+
+my $argssize;
+my @args;
+
+$argssize = scalar @ARGV;
+
+if ( $argssize != 1 ){
+  print STDERR "This script only accepts one arg.\n";
+  exit -1;
+}
+
+if ( $ARGV[0] =~ /[[:alpha:]]/ ){
+  print STDERR "Input must contain only numbers.\n";
+  exit -1;
+}
+
+
+my @input = ( split( '', $ARGV[0] ) );
+my $inputsize = scalar @input;
+
+#As the imput is a circular list we're going to fake it
+$input[$inputsize] = $input[0];
+
+my $result = 0;
+
+for ( my $i = 0; $i < $inputsize ; $i++)
+{
+  if ( $input[$i] == $input[$i+1] )
+  {
+    $result += $input[$i];
+  }
+}
+
+print "Result => $result\n";
+exit 0;
